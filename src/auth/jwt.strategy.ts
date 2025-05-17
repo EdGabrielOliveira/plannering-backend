baa4c85cba78daa2d7a2fdec5ea265d6501fada6
edgabrieloliveira <edgabrieloliveira@proton.me>
2025-05-17 15:11:35 -0300

    fix: test name user

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 interface JwtPayload {
   sub: string;
   email: string;
-  name: string;
+  nome: string;
 }
 
 @Injectable()
@@ -21,6 +22,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(payload: JwtPayload) {
-    return { id: payload.sub, email: payload.email, name: payload.name };
+    return { id: payload.sub, email: payload.email, nome: payload.nome };
   }
 }
