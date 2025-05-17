dc2c9681422462b9feb38982801f6b03485690b3
edgabrieloliveira <edgabrieloliveira@proton.me>
2025-05-17 17:48:12 -0300

    fix: user

--- src/decorators/get-current-user.decorator.ts
+++ src/decorators/get-current-user.decorator.ts
@@ -1,9 +1,18 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 import { Request } from 'express';
 
+interface RequestWithUser extends Request {
+  user?: { id: string; email: string; nome: string };
+}
+
 export const GetCurrentUserId = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
-    const request = context.switchToHttp().getRequest<Request>();
-    return request.user?.id;
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+
+    if (!request.user) {
+      throw new Error('Usuário não encontrado. O JwtAuthGuard está aplicado?');
+    }
+
+    return request.user.id;
   },
 );
